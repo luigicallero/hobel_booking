b85f14f4500e5a97740d8689e40a384834451a8b
luigicallero <58836287+luigicallero@users.noreply.github.com>
2021-05-14 14:43:44 -0300

    Create hotel_booking.sol

---
+++ hotel_booking.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.0;
+// SPDX-License-Identifier: UNLICENSED;
+
+contract HotelRoom {
+    enum Statuses { Vacant, Occupied }
+    Statuses currentStatus;
+    
+    event Occupy(address _occupant, uint _value);
+    
+    address payable public owner;
+    
+    
+    constructor() public{
+        owner = msg.sender;
+        currentStatus = Statuses.Vacant;
+    }
+    
+    modifier onlyWhileVacant{
+        require(currentStatus == Statuses.Vacant, "No available Room");
+        _;
+    }
+    
+    modifier costs(uint _amount){
+        require(msg.value >= _amount, "Not enough Ether provided");
+        _;
+    }
+    
+    receive() external payable onlyWhileVacant costs(2 ether){
+        currentStatus = Statuses.Occupied;
+        owner.transfer(msg.value);
+        emit Occupy(msg.sender, msg.value);
+        
+    }
+}
